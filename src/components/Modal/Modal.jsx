f2bf816c4f60065c91c91eef690e96d046e43740
Nurlana <nurlana@cubics.technology>
2024-03-05 18:29:50 +0400

    test

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,20 +1,23 @@
 import Input from "../Input/Input";
 import Button from "../Button/Button";
+import {useState} from "react";
 
-const Modal=({onClose,singleData,changeEdit})=>{
-   function submitEdit(e){
-       e.preventDefault()
-       singleData.name = e.target.name.value;
-       singleData.desc = e.target.desc.value;
-       changeEdit(singleData)
-       onClose(false)
-   }
-    return(
+const Modal = ({onClose, singleData, changeEdit}) => {
+    function submitEdit(e) {
+        e.preventDefault()
+        singleData.name = e.target.name.value;
+        singleData.desc = e.target.desc.value;
+        changeEdit(singleData)
+        onClose(false)
+    }
+
+    return (
         <>
             <div className="modal_box">
+                <h2>Edit data</h2>
                 <form action="" onSubmit={submitEdit}>
-                    <Input  name={'name'} />
-                    <Input  name={'desc'} />
+                    <Input placeholder={'Name'}  name={'name'}/>
+                    <Input placeholder={'Description'}  name={'desc'}/>
                     <Button title={'Edit'} type={'submit'}/>
                 </form>
             </div>
